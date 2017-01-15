21228704bead61fab67c9c52d4cca9c92eff748d
Rob Glazebrook <rglazebrook@gmail.com>
2017-01-15 14:11:27 -0600

    Removing some unused packages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,6 @@
 'use strict';
 
-// A required polyfill for Node 0.10 in order to get postcss working.
-var Promise = require('es6-promise').Promise;
-
 module.exports = function(grunt) {
-
-// A required polyfill for Node 0.10 in order to get postcss working.
-var Promise = require('es6-promise').Promise;
 
   // Project configuration.
   grunt.initConfig({
@@ -19,31 +13,12 @@ var Promise = require('es6-promise').Promise;
           keepalive: true
         }
       }
-    },
-    watch: {
-      styles: {
-        files: '_site/css/*.css',
-        tasks: ['postcss']
-      }
-    },
-    postcss: {
-      options: {
-
-        processors: [
-          require('autoprefixer')({browsers: 'last 4 versions'}), // add vendor prefixes
-        ]
-      },
-      dist: {
-        src: '_site/css/*.css'
-      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-postcss');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   
   // Default task(s).
-  grunt.registerTask('default', ['postcss']);
+  grunt.registerTask('default', ['connect']);
 
 };
